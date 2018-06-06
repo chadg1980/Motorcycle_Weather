bd2641cb021a6e8b66bc60df977ac2d4d85f9262
chadg1980 <chadg@healthslate.com>
2018-06-05 22:43:06 -0700

    updated the index.handler

--- publish.sh
+++ publish.sh
@@ -3,5 +3,4 @@ rm index.zip
 cd lambda
 zip -X -r ../index.zip *
 cd ..
-touch hello.world
-aws lambda update-function-code --function-name motorcycle_weather --zip-file fileb://index.zip --publish
+aws lambda update-function-code --function-name motorcyle_weather --zip-file fileb://index.zip
